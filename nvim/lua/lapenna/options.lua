ad1a18202033014a8a05100d17be433b54f1fa84
LaPenna <kl.lapenna@gmail.com>
2024-11-20 10:10:59 +0000

    add nvim

---
+++ nvim/lua/lapenna/options.lua
@@ -0,0 +1,34 @@
+vim.opt.termguicolors = true
+vim.opt.title = true
+
+vim.opt.expandtab = false
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.wildmode = 'longest:full,full' -- easier tabbing autocomplete commands
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.mouse = 'a'
+
+vim.opt.list = true
+vim.opt.listchars = { tab = '▸ ', trail = '·', nbsp = '␣' }
+vim.opt.fillchars:append({ eob = ' ' })
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- vim.opt.scrolloff = 10
+-- vim.opt.sidescrolloff = 10
+
+vim.keymap.set('n', 'j', 'jzz', { noremap = true })
+vim.keymap.set('n', 'k', 'kzz', { noremap = true })
+
+print('heeeeeelo')
