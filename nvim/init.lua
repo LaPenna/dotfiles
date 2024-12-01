00540f74bad5dcd228afe64e2a43d8df7e63a54d
LaPenna <kl.lapenna@gmail.com>
2024-12-01 20:08:44 +0000

    change tmux window_name on nvim buffer change

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,22 @@
 require('lapenna/options')
 require('lapenna/keymaps')
 require('lapenna/commands')
+
+if vim.fn.exists('$TMUX') == 1 then
+  -- Rename tmux window when entering a buffer
+  vim.api.nvim_create_autocmd("BufEnter", {
+    callback = function()
+      local filename = vim.fn.expand("%:t")
+      vim.fn.system("tmux rename-window '" .. filename .. "'")
+    end
+  })
+
+  -- Restore tmux automatic window renaming when leaving Vim
+  vim.api.nvim_create_autocmd("VimLeave", {
+    callback = function()
+      vim.fn.system("tmux setw automatic-rename")
+    end
+  })
+end
+require('lapenna/commands')
+
