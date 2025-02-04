896bdaf62c30d6d704a7d6fdb9a3ce7cc0106b0b
LaPenna <kl.lapenna@gmail.com>
2025-02-04 16:16:17 +0000

    add commands for packer

--- nvim/lua/lapenna/commands.lua
+++ nvim/lua/lapenna/commands.lua
@@ -1,2 +1,4 @@
 vim.api.nvim_create_user_command('Q', 'q!', {})
 vim.api.nvim_create_user_command('E', 'e!', {})
+vim.api.nvim_create_user_command('PS', ':PackerSync', {})
+vim.api.nvim_create_user_command('PC', ':PackerCompile', {})
